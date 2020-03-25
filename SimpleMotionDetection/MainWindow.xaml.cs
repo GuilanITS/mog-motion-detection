6827555257e3c775bbe3e87c7a2616234e7250e2
Ali Tourani <a.tourani1991@gmail.com>
2020-03-25 18:08:54 +0430

    feat: add initial design

--- SimpleMotionDetection/MainWindow.xaml.cs
+++ SimpleMotionDetection/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows;
+using Microsoft.Win32;
 using Emgu.CV;
 
 namespace SimpleMotionDetection
@@ -11,6 +12,8 @@ namespace SimpleMotionDetection
     {
         // Global Vaiables
         VideoCapture capturedVideo;                 // Capture Video
+        double TotalFrames = 0;                     // Video's Total Freams
+        double FrameCounter = 0;                    // Video's Frame-rate
         Mat originalFrame;
 
         public MainWindow()
@@ -20,17 +23,47 @@ namespace SimpleMotionDetection
 
         private void BrowseButton_Click(object sender, RoutedEventArgs e)
         {
-            
+            // When the user clicks on the "Load Video" button
+            OpenFileDialog loadVideoDialog = new OpenFileDialog();
+            loadVideoDialog.Filter = "Video files (*.avi, *.mkv, *.mp4) | *.avi; *.mkv; *.mp4";
+            loadVideoDialog.ShowDialog();
+            try
+            {
+                if (loadVideoDialog.FileName != "")
+                {
+                    LoadVideoInit();
+                    capturedVideo = new VideoCapture(loadVideoDialog.FileName);
+                    originalFrame = new Mat();
+                    capturedVideo.ImageGrabbed += ProcessVideo;
+                    capturedVideo.Start();
+                }
+                else
+                {
+                    return;
+                }
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Something went wrong!\n" + err.ToString(), "Error!", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+            }
         }
 
 
         private void LoadVideoInit()
         {
-
+            TotalFrames = Convert.ToDouble(capturedVideo.GetCaptureProperty(Emgu.CV.CvEnum.CapProp.FrameCount));
         }
 
         private void ProcessVideo(object sender, EventArgs e)
         {
+            FrameCounter++;
+            // Check the end of video
+            if (FrameCounter == TotalFrames)
+            {
+                capturedVideo.Dispose();
+                return;
+            }
+            capturedVideo.Retrieve(originalFrame);
         }
     }
 }
